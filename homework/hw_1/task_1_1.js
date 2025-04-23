1aa51c23a7dee292a7402fa3c4ab5f44571cf284
Maks Onyshchenko <onyshchenkomaksymm@gmail.com>
2025-04-23 12:32:35 +0300

    hw_1

---
+++ homework/hw_1/task_1_1.js
@@ -0,0 +1,6 @@
+let firstName = "Maksym";
+let middleName = "Mykolayovych";
+let lastName = "Onyshchenko";
+
+let person = firstName + " " + middleName + " " + lastName;
+console.log(person);
---
+++ homework/hw_1/task_1_3.js
@@ -0,0 +1,7 @@
+let firstName = "Максим";
+let middleName = "Миколайович";
+let age = "36 років";
+
+prompt("Ім'я", firstName);
+prompt("По-батькові", middleName);
+prompt("Вік", age);
